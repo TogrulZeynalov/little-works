6e89e3da7a623856406b329951fdc8c6a5025edf
Toğrul Zeynalov <ztogrul545@gmail.com>
2025-01-31 23:22:55 +0400

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,17 +1,12 @@
 import turtle
 n = input().split(' ')
-s = ['1','2','3','4','5','6','7','8','9']
-r = 0
-for i in n:
-    if s.count(i) != 0:
-        r = i
 if n.count('square') != 0:
-    turtle.forward(r)
+    turtle.forward(150)
     turtle.right(90)
-    turtle.forward(r)
+    turtle.forward(150)
     turtle.right(90)
-    turtle.forward(r)
+    turtle.forward(150)
     turtle.right(90)
-    turtle.forward(r)
+    turtle.forward(150)
     turtle.right(90)
     
