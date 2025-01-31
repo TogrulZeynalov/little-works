f0c3409fb0fbc1abe154720c6bdc704fc9106e59
Toğrul Zeynalov <ztogrul545@gmail.com>
2025-01-31 23:00:55 +0400

    Update sample.py
    added feature to change square's size

--- sample.py
+++ sample.py
@@ -1,11 +1,16 @@
 import turtle
 n = input().split(' ')
+s = ['1','2','3','4','5','6','7','8','9']
+for i in n:
+    if s.count(i) != 0:
+        r = i
 if n.count('square') != 0:
-    turtle.forward(100)
+    turtle.forward(r)
     turtle.right(90)
-    turtle.forward(100)
+    turtle.forward(r)
     turtle.right(90)
-    turtle.forward(100)
+    turtle.forward(r)
     turtle.right(90)
-    turtle.forward(100)
+    turtle.forward(r)
     turtle.right(90)
+    
