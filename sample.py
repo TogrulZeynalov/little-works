74dcf246cebfbf048bb15cb81d179ad0c7c40fb6
Toğrul Zeynalov <ztogrul545@gmail.com>
2025-02-01 22:31:24 +0400

    Browser update
    added a browser function that searches given input on the internet with your default browser

--- sample.py
+++ sample.py
@@ -1,12 +1,24 @@
-import turtle
-n = input().split(' ')
-if n.count('square') != 0:
-    turtle.forward(150)
-    turtle.right(90)
-    turtle.forward(150)
-    turtle.right(90)
-    turtle.forward(150)
-    turtle.right(90)
-    turtle.forward(150)
-    turtle.right(90)
-    
+def browser():
+    n = input()
+    import webbrowser
+    import requests
+    from bs4 import BeautifulSoup
+    url = "https://www.google.com/search?q=" + n
+    def open_website():
+        webbrowser.open(url)
+    if __name__ == "__main__":
+        open_website()
+
+def square():
+    import turtle
+    n = input().split(' ')
+    if n.count('square') != 0:
+        turtle.forward(150)
+        turtle.right(90)
+        turtle.forward(150)
+        turtle.right(90)
+        turtle.forward(150)
+        turtle.right(90)
+        turtle.forward(150)
+        turtle.right(90)
+square()
